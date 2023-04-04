7a0bb939dc3c814f39cd60176d8b501efbaf1906
@josegabo21 <josegabriel2.jr78@gmail.com>
2023-04-04 03:33:24 -0430

    challenge 03

---
+++ js/validaciones.js
@@ -0,0 +1,15 @@
+const usuario = document.getElementById('usuario');
+const contrasena = document.getElementById('password');
+const boton = document.getElementById('boton');
+const form = document.getElementById('form');
+const error = document.getElementById('error');
+
+
+function enviarMensaje(){
+    
+  let redireccion = false;
+
+
+}
+
+  
